c74f5329b90cbd2adb524cdb1219564b6d5583fc
channa <cdiasperera@gmail.com>
2023-08-01 09:43:42 +0200

    Restarted project

---
+++ cmake-build-debug/CMakeFiles/cppHelper.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cppHelper.dir/main.cpp.o"
+  "CMakeFiles/cppHelper.dir/main.cpp.o.d"
+  "cppHelper"
+  "cppHelper.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cppHelper.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/cppHelper.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for cppHelper.
+# This may be replaced when dependencies are built.
